293f4b9e96dd6481b2fb72cd24262f4fda95f4cb
Brendan Chen <smallpigeon1@gmail.com>
2023-05-17 11:47:02 -0700

    remove e argument from lambda

--- exercise1.py
+++ exercise1.py
@@ -11,10 +11,10 @@ F_X = lambda x, y : (2 * x) + np.sin(10 * y) - (4 * np.sin(4 * x))
 F_Y = lambda x, y : (2 * y) + (10 * x) * np.cos(10 * y)
 
 # Minimise the gradient
-min_F = lambda x, y, e : (F_X(x, y) * -e, F_Y(x, y) * -e)
+min_F = lambda x, y : (F_X(x, y) * -LEARN_RATE, F_Y(x, y) * -LEARN_RATE)
 
 # Maximise the gradient
-max_F = lambda x, y, e : (F_X(x, y) * e, F_Y(x, y) * e)
+max_F = lambda x, y : (F_X(x, y) * LEARN_RATE, F_Y(x, y) * LEARN_RATE)
 
 # Approximate
 guess = INITIAL_GUESS
@@ -22,7 +22,7 @@ guess = INITIAL_GUESS
 i = 0
 step_size = (float('inf'), float('inf'))
 while i < CYCLES and np.abs(step_size[0]) >= LEARN_RATE and np.abs(step_size[1]) >= LEARN_RATE:
-  step_size = min_F(guess[0], guess[1], LEARN_RATE)
+  step_size = min_F(guess[0], guess[1])
   # print(step_size)
 
   guess = (guess[0] + step_size[0], guess[1] + step_size[1])
