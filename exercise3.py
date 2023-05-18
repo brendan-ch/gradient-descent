32c42e214ddccb7f8aca7cea42fdf2c9f056bb24
Brendan Chen <smallpigeon1@gmail.com>
2023-05-17 20:26:16 -0700

    add constant to control maximum number of sweep cycles

--- exercise3.py
+++ exercise3.py
@@ -2,7 +2,8 @@ import numpy as np
 
 # Constants, change these as needed
 INITIAL_GUESS = (2, 3)  # initial guess as stated by the exercise
-CYCLES = 10000  # maximum computational cycles
+DESCENT_CYCLES = 10000  # maximum descent cycles
+SWEEP_CYCLES = 10000  # maximum sweep cycles
 LEARN_RATE = 0.00001  # the learning rate, indicated by epsilon in problem
 H = 0.00001  # differential quotient; smaller is more accurate
 RANGE = 2  # range of neighboring points to check after the initial point
@@ -18,27 +19,32 @@ F_Y = lambda x, y : (F(x, y + H) - F(x, y - H)) / (2 * H)
 min_F = lambda x, y : (F_X(x, y) * -LEARN_RATE, F_Y(x, y) * -LEARN_RATE)
 max_F = lambda x, y : (F_X(x, y) * LEARN_RATE, F_Y(x, y) * LEARN_RATE)
 
+# Descent function
 def descend(initial_guess):
   # Approximate
   guess = initial_guess
 
   i = 0
   step_sizes = (float('inf'), float('inf'))
-  while i < CYCLES and np.abs(step_sizes[0]) >= LEARN_RATE and np.abs(step_sizes[1]) >= LEARN_RATE:
+  while i < DESCENT_CYCLES and np.abs(step_sizes[0]) >= LEARN_RATE and np.abs(step_sizes[1]) >= LEARN_RATE:
     step_sizes = min_F(guess[0], guess[1])
     # print(step_sizes)
 
+    # Move guess towards the step size
     guess = (guess[0] + step_sizes[0], guess[1] + step_sizes[1])
     i += 1
 
+  # Return final guess
   return guess
 
+# Compute the initial min point
 min_point = descend(INITIAL_GUESS)
-prev_min_point = (0, 0)
-min_value = F(min_point[0], min_point[1])
+prev_min_point = (0, 0)  # This is used to check if min_point has changed
+min_value = F(min_point[0], min_point[1])  # Calculate the minimum value at the point
 
-# Get the neighboring points
-while (min_point != prev_min_point):
+# "Sweep" and check the neighboring points
+i = 0
+while i < SWEEP_CYCLES and min_point != prev_min_point:
   prev_min_point = min_point
   for x in range(int(np.round(min_point[0] - RANGE)), int(np.round(min_point[0] + RANGE))):
     for y in range(int(np.round(min_point[1] - RANGE)), int(np.round(min_point[1] + RANGE))):
@@ -46,6 +52,8 @@ while (min_point != prev_min_point):
       if F(guess[0], guess[1]) < min_value:
         min_point = guess
         min_value = F(guess[0], guess[1])
+
+  i += 1
   
 
 print(min_point)
